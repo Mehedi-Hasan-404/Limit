db86dda4a9f9bc170f27c5ea007b1098186fffe4
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-27 12:06:48 +0600

    Update GlideExtensions.kt

--- app/src/main/java/com/livetvpro/app/utils/GlideExtensions.kt
+++ app/src/main/java/com/livetvpro/app/utils/GlideExtensions.kt
@@ -1,10 +1,15 @@
 package com.livetvpro.app.utils
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
-import coil.load
-import coil.request.CachePolicy
-import coil.size.Size
-import coil.transform.CircleCropTransformation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
 object GlideExtensions {
 
@@ -21,14 +26,41 @@ object GlideExtensions {
             return
         }
 
-        imageView.load(url) {
-            diskCachePolicy(CachePolicy.ENABLED)
-            memoryCachePolicy(CachePolicy.ENABLED)
-            crossfade(false)
-            size(Size.ORIGINAL)
-            if (placeholderResId != null) placeholder(placeholderResId)
-            if (errorResId != null) error(errorResId)
-            if (isCircular) transformations(CircleCropTransformation())
+        val isSvg = url.contains(".svg", ignoreCase = true)
+
+        if (isSvg) {
+            val request = Glide.with(imageView.context)
+                .`as`(Drawable::class.java)
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>, isFirstResource: Boolean): Boolean {
+                        if (errorResId != null) imageView.setImageResource(errorResId)
+                        return true
+                    }
+                    override fun onResourceReady(resource: Drawable, model: Any, target: Target<Drawable>, dataSource: DataSource, isFirstResource: Boolean): Boolean {
+                        return false
+                    }
+                })
+
+            if (isCircular) {
+                request.apply(RequestOptions.bitmapTransform(CircleCrop())).into(imageView)
+            } else {
+                request.into(imageView)
+            }
+        } else {
+            var options = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .skipMemoryCache(false)
+
+            if (placeholderResId != null) options = options.placeholder(placeholderResId)
+            if (errorResId != null) options = options.error(errorResId)
+            if (isCircular) options = options.transform(CircleCrop())
+
+            Glide.with(imageView.context)
+                .load(url)
+                .apply(options)
+                .into(imageView)
         }
     }
 }
