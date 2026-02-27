371e8e41958ab173565ba7f290290bf3ba96d029
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-27 15:01:40 +0600

    Update PlayerActivity.kt

--- app/src/main/java/com/livetvpro/app/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/livetvpro/app/ui/player/PlayerActivity.kt
@@ -588,6 +588,7 @@ class PlayerActivity : AppCompatActivity() {
         }
 
         controlsState.hide()
+        binding.playerControlsCompose.visibility = View.GONE
 
         setupPipReceiver()
 
@@ -607,18 +608,6 @@ class PlayerActivity : AppCompatActivity() {
         binding.playerControlsCompose.visibility = View.VISIBLE
 
         if (!isLandscape) {
-            val params = binding.playerContainer.layoutParams as ConstraintLayout.LayoutParams
-            params.width = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
-            params.height = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
-            params.dimensionRatio = "H,16:9"
-            params.topMargin = 0
-            params.bottomMargin = 0
-            params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID
-            params.bottomToBottom = ConstraintLayout.LayoutParams.UNSET
-            params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
-            params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
-            binding.playerContainer.layoutParams = params
-
             val hasRelated = relatedChannels.isNotEmpty() ||
                 (contentType == ContentType.EVENT && ::relatedEventsAdapter.isInitialized)
             if (hasRelated) {
@@ -1822,24 +1811,7 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun prepareUIForPip() {
-        binding.relatedChannelsSection.visibility = View.GONE
-        binding.linksSection.visibility = View.GONE
-        binding.messageBannerContainer.visibility = View.GONE
-        binding.playerControlsCompose.visibility = View.GONE
         controlsState.hide()
-
-        val params = binding.playerContainer.layoutParams as ConstraintLayout.LayoutParams
-        params.width = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
-        params.height = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
-        params.dimensionRatio = null
-        params.topMargin = 0
-        params.bottomMargin = 0
-        params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID
-        params.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
-        params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
-        params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
-        binding.playerContainer.layoutParams = params
-        binding.playerContainer.requestLayout()
     }
 
     @SuppressLint("NewApi")
