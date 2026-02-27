d3a6880d62146afa2a50057cd5bea2a707f422d2
Mehedi-Hasan-404 <880188mm@gmail.com>
2026-02-27 17:26:43 +0600

    Update LiveTVProApplication.kt

--- app/src/main/java/com/livetvpro/app/LiveTVProApplication.kt
+++ app/src/main/java/com/livetvpro/app/LiveTVProApplication.kt
@@ -34,6 +34,7 @@ class LiveTVProApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DeviceUtils.init(this)
+        DeviceUtils.applyUiModeOverride(preferencesManager.getUiMode())
         FloatingPlayerManager.initialize(preferencesManager)
         themeManager.applyTheme()
 
